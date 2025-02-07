b666d40e06bf2c2f5d9780257cb596a9f12810fd
Nagarajan Vivek Raj <nagarajan-vivek-raj@dxc.com>
2025-02-07 15:43:14 +0530

    Added PayTheBill game.

---
+++ Pages/PayTheBill.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dotnetcoresample.Pages
+{
+    public class PayTheBillModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
